c9933ad34716a84359e4c997bcb271e258a4f665
nacong <nacong@ucsc.edu>
2021-10-03 23:15:19 +0000

    fifth debugging attempt

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -15,13 +15,13 @@ int main(void) {
     int points[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     int player_counter = 0;
 
-    fprintf(stderr, "How many players? ");
+    printf("How many players? ");
     scanf("%d", &players);
     if (players < 2 || players > 10) {
         fprintf(stderr, "Invalid number of players. Using 2 instead.\n");
         players = 2;
     }
-    fprintf(stderr, "Random seed: ");
+    printf("Random seed: ");
     scanf("%d", &seed);
     if (seed < 0 || seed > UINT_MAX) {
         fprintf(stderr, "Invalid random seed. Using 2021 instead.\n");
@@ -30,7 +30,7 @@ int main(void) {
 
     srand(seed);
 
-    fprintf(stderr, "%s rolls the pig...", names[player_counter]);
+    printf("%s rolls the pig...", names[player_counter]);
 
     while (points[player_counter] < 100) {
 
@@ -41,26 +41,26 @@ int main(void) {
             if (player_counter >= players) {
                 player_counter = 0;
             }
-            fprintf(stderr, "pig lands on side \n");
-	    fprintf(stderr, "%s rolls the pig...", names[player_counter]);
+            printf(" pig lands on side\n");
+            printf("%s rolls the pig...", names[player_counter]);
         }
         if (pig[rng] == RAZORBACK) {
-            fprintf(stderr, "pig lands on back ");
+            printf(" pig lands on back");
             points[player_counter] = points[player_counter] + 10;
         }
         if (pig[rng] == TROTTER) {
-            fprintf(stderr, "pig lands upright ");
+            printf(" pig lands upright");
             points[player_counter] = points[player_counter] + 10;
         }
         if (pig[rng] == SNOUTER) {
-            fprintf(stderr, "pig lands on snout ");
+            printf(" pig lands on snout");
             points[player_counter] = points[player_counter] + 15;
         }
         if (pig[rng] == JOWLER) {
-            fprintf(stderr, "pig lands on ear ");
+            printf(" pig lands on ear");
             points[player_counter] = points[player_counter] + 5;
         }
     }
-    fprintf(stderr, "\n%s wins with %d points!\n", names[player_counter], points[player_counter]);
+    printf("\n%s wins with %d points!\n", names[player_counter], points[player_counter]);
     return 0;
 }
