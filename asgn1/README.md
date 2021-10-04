b0f7fd6ca1617e150cfb16c58a51946013cda127
nacong <nacong@ucsc.edu>
2021-10-04 06:24:44 +0000

    Completed README.md and added comments to pig.c

---
+++ asgn1/README.md
@@ -0,0 +1,20 @@
+
+# Assignment 1 - Pass the Pigs - README
+
+## Program Description
+
+This program runs a version of the dice game Pass the Pigs, by David Moffat.  This simplified form of the game is played with n number of players, ranging from two to ten players. Arranged in what can be visualized as a circle, the players roll the dice, or the pig, in an effort to earn points. Whenever the pig lands on its side, the next player’s turn commences. The first player to reach 100 points wins the game. 
+
+## Program Compiling
+
+To compile the program into an executable file, input the following command: *make all*
+Following this an executable binary file, titled "pig" will be generated in the directory.
+
+## How to run this program
+
+After following the compiling instructions above, input the following command: *./pig*
+The program will proceed to begin and you will be asked to input a valid number of players, followed by a valid seed number for the random number generator.
+
+## Known bugs
+
+When a string is inputted, the error message for an invalid number of players is outputted but not the error message for an invalid seed. The program still proceeds as normal despite this.
--- asgn1/pig.c
+++ asgn1/pig.c
@@ -4,26 +4,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SEED 2021
-
-typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER } Position;
-const Position pig[7] = { SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER };
+typedef enum {
+    SIDE,
+    RAZORBACK,
+    TROTTER,
+    SNOUTER,
+    JOWLER
+} Position; /* declaration of names of enumerated positions of array below */
+const Position pig[7] = { SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER,
+    JOWLER }; /* enumerated array for pig positions */
 
 int main(void) {
     int players = 0;
     unsigned int seed = 2021;
-    int points[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int points[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0 }; /* points array for logging points accumulated by players */
     int player_counter = 0;
 
     printf("How many players? ");
     scanf("%d", &players);
-    if (players < 2 || players > 10) {
+    if (players < 2 || players > 10) { /* checks for a valid number of players */
         fprintf(stderr, "Invalid number of players. Using 2 instead.\n");
         players = 2;
     }
     printf("Random seed: ");
     scanf("%d", &seed);
-    if (seed < 0 || seed > UINT_MAX) {
+    if (seed < 0 || seed > UINT_MAX) { /* checks if seed is in the valid range */
         fprintf(stderr, "Invalid random seed. Using 2021 instead.\n");
         seed = 2021;
     }
@@ -32,11 +38,14 @@ int main(void) {
 
     printf("%s rolls the pig...", names[player_counter]);
 
-    while (points[player_counter] < 100) {
+    while (points[player_counter] < 100) { /* while any player's score is under 100 points */
 
-        int rng = random() % 7;
+        int rng = random()
+                  % 7; /* usage of modulus operations to keep number generated between 0 and 6 */
 
-        if (pig[rng] == SIDE) {
+        /* conditionals check position correspondence to number generated */
+
+        if (pig[rng] == SIDE) { /* moves to the next player's turn */
             ++player_counter;
             if (player_counter >= players) {
                 player_counter = 0;
@@ -61,6 +70,7 @@ int main(void) {
             points[player_counter] = points[player_counter] + 5;
         }
     }
-    printf("\n%s wins with %d points!\n", names[player_counter], points[player_counter]);
+    printf("\n%s wins with %d points!\n", names[player_counter],
+        points[player_counter]); /* triggers when 100 or more points are reached */
     return 0;
 }
