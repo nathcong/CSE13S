51a6a09370361ecf82652e66bb547fcd0dbd7a89
nacong <nacong@ucsc.edu>
2021-10-10 12:58:15 +0000

    bbp function done and tested

--- asgn2/bbp.c
+++ asgn2/bbp.c
@@ -1,3 +1,28 @@
 #include "mathlib.h"
-
 #include <stdio.h>
+
+static int total_bbp_terms = 1;
+
+double pi_bbp(void) {
+        double fraction = 0.0;
+        double exponential_fraction = 0.0;
+        double current = 47.0/15.0;
+        double previous = 0.0;
+        unsigned long exponent = 1.0;
+        int k = 1;
+        while (absolute(current - previous) > EPSILON) {
+                exponent *= 16.0;
+                exponential_fraction = 1.0 / exponent;
+                fraction =((k * (120.0 * k + 151.0)) + 47.0)/((((k * ((k * ((k * (512 * k + 1024)) + 712)) + 194))) + 15));
+                previous = current;
+                current += (exponential_fraction * fraction);
+                k++;
+                total_bbp_terms++;
+        }
+        return current;
+}
+
+int pi_bbp_terms(void) {
+        return total_bbp_terms;
+}
+
