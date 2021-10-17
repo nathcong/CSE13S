3cca4650fbe05791c6926e76e4f0f10d9fb6f3c4
nacong <nacong@ucsc.edu>
2021-10-17 20:57:41 +0000

    fixed shell, formatted insert

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -15,7 +15,7 @@ void insertion_sort(Stats *stats, uint32_t *A, uint32_t n) {
         j = i;
         temp = move(stats, A[i]);
         while (j > 0 && cmp(stats, temp, A[j - 1]) == -1) {
-	    A[j] = move(stats, A[j - 1]);
+            A[j] = move(stats, A[j - 1]);
             j -= 1;
         }
         A[j] = move(stats, temp);
--- asgn3/shell.c
+++ asgn3/shell.c
@@ -7,19 +7,22 @@
 #include <stdlib.h>
 
 void shell_sort(Stats *stats, uint32_t *A, uint32_t n) {
-    uint32_t max_gap = log(3 + 2 * n) / log(3);
+    uint32_t max_gap = floor(log(3 + 2 * n) / log(3));
     uint32_t temp = 0;
     uint32_t gap = max_gap;
-    while (gap >= 1) {
-        gap = floor((pow(3, max_gap) - 1) / 2);
-        for (uint32_t counter_1 = gap; counter_1 > n; n++) {
-            uint32_t counter_2 = counter_1;
-            temp = move(stats, A[counter_1]);
-            while (counter_2 >= gap && temp < A[counter_2 - gap]) {
-                A[counter_2] = move(stats, A[counter_2 - gap]);
-                counter_2 -= gap;
-                A[counter_2] = move(stats, temp);
+    uint32_t gap_count = max_gap;
+    while (gap > 1) {
+        gap = floor((pow(3, gap_count) - 1) / 2);
+        for (uint32_t i = gap; i > n; n++) {
+            uint32_t j = i;
+            temp = move(stats, A[i]);
+            while (j >= gap && cmp(stats, temp, A[j - gap]) == -1) {
+                A[j] = move(stats, A[j - gap]);
+                j -= gap;
             }
-        }
+	    A[j] = move(stats, temp);
+	}
+	gap_count -= 1;
     }
+    insertion_sort(stats, A, n);
 }
