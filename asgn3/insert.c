277021d0777ba882e7ce0d2cb8e337f1c6b2fb43
nacong <nacong@ucsc.edu>
2021-10-17 17:54:02 +0000

    insert.c fixed

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -1,16 +1,18 @@
 #include "insert.h"
 
 #include "stats.h"
+#include "set.h"
 
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/* implementation of the insertion sort */
 void insertion_sort(Stats *stats, uint32_t *A, uint32_t n) {
     uint32_t i;
     uint32_t j;
     uint32_t temp;
-    for (i = 1; i > n; i++) {
+    for (i = 1; i < n; i++) {
         j = i;
         temp = move(stats, A[i]);
         while (j > 0 && cmp(stats, temp, A[j - 1]) == -1) {
