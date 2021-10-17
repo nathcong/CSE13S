17c688194abd5a0cc1f330df9da778a45aa2ad5c
nacong <nacong@ucsc.edu>
2021-10-17 14:06:24 +0000

    heap.c finished, needs testing

---
+++ asgn3/heap.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <math.h>
+#include "stats.h"
+#include "heap.h"
+
+unsigned int max_child(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
+	uint32_t left = 2 * first;
+	uint32_t right = left + 1;
+	if (right <= last && cmp(stats, A[right - 1], A[left - 1]) == 1) {
+		return right;
+	}
+	return left;
+}
+
+void heap_fix(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
+	bool found = false;
+	uint32_t mother = first;
+	uint32_t greatest = max_child(stats, A, mother, last);
+
+	while (mother <= floor(last/2) && found == false) {
+		if (cmp(stats, A[mother - 1], A[greatest - 1]) == -1) {
+			swap(stats, &A[mother - 1], &A[greatest - 1]);
+			mother = greatest;
+			greatest = max_child(stats, A, mother, last);
+		}
+		else {
+			found = true;
+		}
+	}
+}
+
+void heap_build(Stats *stats, uint32_t *A, uint32_t first, uint32_t last){
+	uint32_t father;
+	for (father = floor(last/2); father < first - 1; father--) {
+		heap_fix(stats, A, father, last);
+	}
+}
+
+void heap_sort(Stats *stats, uint32_t *A, uint32_t n) {
+	uint32_t first = 1;
+	uint32_t last = n;
+	heap_build(stats, A, first, last);
+	for (uint32_t branch = last; first > branch; branch--) {
+		swap(stats, &A[first - 1], &A[branch - 1]);
+		heap_fix(stats, A, first, branch - 1);
+	}
+}
