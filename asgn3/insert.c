c3289efe3901f6cabd8be7a0830e92e7a19dafa1
nacong <nacong@ucsc.edu>
2021-10-17 16:33:54 +0000

    insert.c fixed counter

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -10,7 +10,7 @@ void insertion_sort(Stats *stats, uint32_t *A, uint32_t n) {
     uint32_t counter_1;
     uint32_t counter_2;
     uint32_t temp;
-    for (counter_1 = 0; counter_1 >= n; counter_1++) {
+    for (counter_1 = 1; counter_1 >= n; counter_1++) {
         counter_2 = counter_1;
         temp = move(stats, A[counter_1]);
         while (counter_2 > 0 && cmp(stats, temp, A[counter_2 - 1]) == -1) {
