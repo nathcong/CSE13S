b44ae5e478cc1f2ddb19b98c1808ed935154e1f4
nacong <nacong@ucsc.edu>
2021-10-23 19:04:25 +0000

    graph.c up to graph print, stack.c inputted provided code and struct

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -23,23 +23,63 @@ uint32_t graph_vertices(Graph *G) {
 	return G->vertices;
 }
 bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
-
+	if (0 <= i < G->vertices && 0 <= j < G->vertices) {
+		G->matrix[i][j] = k;
+		if (G->undirected) {
+			G->matrix[j][i] = k;
+		}
+		return true;
+	}
+	else {
+		return false;
+	}
 }
+
 bool graph_has_edge(Graph *G, uint32_t i, uint32_t j) {
-
+	if (0 <= i < G->vertices && 0 <= j < G->vertices) {
+		if (G->matrix[i][j] > 0) {
+			return true;
+		}
+	}
+	else {
+		return false;
+	}
 }
-uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j) {
 
+uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j) {
+	if (0 <= i < G->vertices && 0 <= j < G->vertices) {
+		if (G->matrix[i][j] > 0) {
+			return G->matrix[i][j];
+		}
+		else {
+			return 0;
+		}
+	}
+	else {
+		return 0;
+	}
 }
 bool graph_visited(Graph *G, uint32_t v) {
-
+	if (v < G->vertices) {
+		if (G->visited[v]) {
+			return true;
+		else {
+			return false;
+		}
+	}
+	return false;
 }
 void graph_mark_visited(Graph *G, uint32_t v) {
-
+	if (v < G->vertices) {	
+		G->visited[v] = true;
+	}
 }
 void graph_mark_unvisited(Graph *G, uint32_t v) {
-
+	if (v < G->vertices) {
+		G->visited[v] = false;
+	}
 }
 void graph_print(Graph *G) {
 
 }
+
--- asgn4/stack.c
+++ asgn4/stack.c
@@ -10,13 +10,33 @@ struct Stack {
 };
 
 Stack *stack_create(uint32_t capacity) {
-
+	Stack *s = (Stack *) malloc(sizeof(stack));
+	if (s) {
+		s->top = 0;
+		s->capacity = capacity;
+		s->items = (uint32_t *) calloc(capacity, sizeof(uint32_t));
+		if (!s->items) {
+			free(s);
+			s = NULL;
+		}
+	}
+	return s;
 }
 void stack_delete(Stack **s) {
-
+	if (*s && (*s)->items) {
+		free((*s)->items);
+		free(*s);
+		*s = NULL;
+	}
+	return;
 }
 uint32_t stack_size(Stack *s) {
-
+	if (s->top == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 bool stack_empty(Stack *s) {
 
