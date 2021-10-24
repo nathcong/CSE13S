9db244bdaaebbdee95781c7b02655c14019d8ab9
nacong <nacong@ucsc.edu>
2021-10-24 09:08:16 +0000

    attempt fix graph

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -31,7 +31,7 @@ uint32_t graph_vertices(Graph *G) {
 bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
     if (0 <= i < G->vertices && 0 <= j < G->vertices) {
         G->matrix[i][j] = k;
-        if (G->undirected) {
+        if (G->undirected == true) {
             G->matrix[j][i] = k;
         }
         return true;
