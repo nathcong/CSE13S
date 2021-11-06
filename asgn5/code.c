84c68759ead13cbf7216890df87f0b4f7f5b52ef
nacong <nacong@ucsc.edu>
2021-11-06 18:44:29 +0000

    attempt fix code

--- asgn5/code.c
+++ asgn5/code.c
@@ -80,9 +80,9 @@ bool code_push_bit(Code *c, uint8_t bit) {
 
 bool code_pop_bit(Code *c, uint8_t *bit) {
     if (c->top != 0) {
+	c->top--;
         *bit = code_get_bit(c, c->top);
         code_clr_bit(c, c->top);
-        c->top--;
         return true;
     } else {
         return false;
