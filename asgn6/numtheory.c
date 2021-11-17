6730dca59116af5a538ae92bbb86691fb52fdc0b
nacong <nacong@ucsc.edu>
2021-11-17 02:34:58 +0000

    attempt fix

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -58,19 +58,15 @@ void mod_inverse(mpz_t i, mpz_t a, mpz_t n) {
         mpz_mul(timest, q, tempt);
         mpz_sub(tempt, originalt, timest);
     }
+    if ((mpz_cmp_ui(t, 0)) < 0) {
+        mpz_add(t, t, n);
+    }
+
+    mpz_set(i, t);
 
     /* no inverse */
     if ((mpz_cmp_ui(r, 1)) > 0) {
         mpz_set(i, 0);
-    }
-
-    if ((mpz_cmp_ui(t, 0)) < 0) {
-        mpz_add(t, t, n);
-	mpz_set(i, t);
-    }
-
-    else {
-	mpz_set(i, t);
     }
 
     /* free memory of temporary variables */
