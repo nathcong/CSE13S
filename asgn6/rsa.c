15cfe06b5bc7fbfd4da04e14c1c0078584dab771
nacong <nacong@ucsc.edu>
2021-11-18 08:09:04 +0000

    rsa work so far

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -1,4 +1,6 @@
 #include "rsa.h"
+#include "randstate.h"
+#include "numtheory.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -6,7 +8,29 @@
 #include <gmp.h>
 
 void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t iters) {
-    ;
+    	mpz_t pbits, qbits, pmultiply, qmultiply, random;
+	mpz_inits(pbits, qbits, random, pmultiply, qmultiply, NULL);
+
+	/* calculate numbers needed for bits that go to p and q */
+	mpz_mul_ui(random, 2, nbits);
+
+	urandomm(pbits, state, random);
+	mpz_add(pbits, pbits, nbits);
+	mpz_fdiv_q_ui(pbits, pbits, 4);
+
+    	mpz_sub(qbits, nbits, pbits);
+
+	/* make prime numbers */
+	make_prime(p, pbits, iters);
+    	make_prime(q, qbits, iters);
+
+	/* n = (p - 1)(q - 1) */
+	mpz_sub_ui(pmultiply, p, 1);
+	mpz_sub_ui(qmultiply, q, 1);
+	mpz_mul(n, pmultiply, qmultiply);
+
+	/* find usable e value */
+
 }
 
 void rsa_write_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
