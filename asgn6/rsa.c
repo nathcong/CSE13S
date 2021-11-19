88dbf27a97fc0418f882b9f3c9cb4c42bcb03969
nacong <nacong@ucsc.edu>
2021-11-19 01:54:45 +0000

    verify

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -108,6 +108,18 @@ void rsa_sign(mpz_t s, mpz_t m, mpz_t d, mpz_t n) {
     pow_mod(s, m, d, n);
 }
 
+/* verifies signature */
 bool rsa_verify(mpz_t m, mpz_t s, mpz_t e, mpz_t n) {
-    ;
+    mpz_t t;
+    mpz_t_inits(t, NULL);
+
+    pow_mod(t, s, e, n);
+
+    /* returns true in t == m */
+    if ((mpz_cmp(t, m)) == 0) {
+	return true;
+    }
+    else {
+	return false;
+    }
 }
