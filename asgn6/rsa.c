5a93a3b1eecc7248036ac60c77cdb2444b776b3b
nacong <nacong@ucsc.edu>
2021-11-19 01:27:09 +0000

    make private key

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -45,6 +45,7 @@ void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t i
     mpz_clears(random, pmultiply, qmultiply, totient, g, NULL);
 }
 
+/* writes public key into pbfile */
 void rsa_write_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
     gmp_fprintf(pbfile, "%Zx\n", n);
     gmp_fprintf(pbfile, "%Zx\n", e);
@@ -52,6 +53,7 @@ void rsa_write_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
     gmp_fprintf(pbfile, "%hh\n", username);
 }
 
+/* reads public key from pbfile */
 void rsa_read_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
     gmp_fscanf(pbfile, "%Zx\n", n);
     gmp_fscanf(pbfile, "%Zx\n", e);
@@ -59,15 +61,27 @@ void rsa_read_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
     gmp_fscanf(pbfile, "%hh\n", username);
 }
 
+/* makes private RSA key */
 void rsa_make_priv(mpz_t d, mpz_t e, mpz_t p, mpz_t q) {
-    ;
+    mpz_t ptemp, qtemp, totient;
+    mpz_init(ptemp, qtemp, totient, NULL);
+
+    mpz_sub_ui(ptemp, p, 1);
+    mpz_sub_ui(qtemp, q, 1);
+    mpz_mul(totient, ptemp, qtemp);
+
+    mod_inverse(d, e, totient);
+
+    mpz_clears(ptemp, qtemp, totient, NULL);
 }
 
+/* writes private key into pvfile */
 void rsa_write_priv(mpz_t n, mpz_t d, FILE *pvfile) {
     gmp_fprintf(pvfile, "%Zx\n", n);
     gmp_fprintf(pvfile, "%Zx\n", d);
 }
 
+/* reads private key from pvfile */
 void rsa_read_priv(mpz_t n, mpz_t d, FILE *pvfile) {
     gmp_fscanf(pvfile, "%Zx\n", n);
     gmp_fscanf(pvfile, "%Zx\n", d);
