f2d9e46a0a0cce4c590fc4c7407856d5ef8062b3
nacong <nacong@ucsc.edu>
2021-11-19 01:17:32 +0000

    write and read priv key

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -64,13 +64,13 @@ void rsa_make_priv(mpz_t d, mpz_t e, mpz_t p, mpz_t q) {
 }
 
 void rsa_write_priv(mpz_t n, mpz_t d, FILE *pvfile) {
-    gmp_fprintf(pbfile, "%Zx\n", n);
-    gmp_fprintf(pbfile, "%Zx\n", d);
+    gmp_fprintf(pvfile, "%Zx\n", n);
+    gmp_fprintf(pvfile, "%Zx\n", d);
 }
 
 void rsa_read_priv(mpz_t n, mpz_t d, FILE *pvfile) {
-    gmp_fscanf(pbfile, "%Zx\n", n);
-    gmp_fscanf(pbfile, "%Zx\n", d);
+    gmp_fscanf(pvfile, "%Zx\n", n);
+    gmp_fscanf(pvfile, "%Zx\n", d);
 }
 
 void rsa_encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n) {
