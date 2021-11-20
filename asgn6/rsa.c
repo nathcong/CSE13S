48401d08a16bd04797001b70fe45df0180b0fa64
nacong <nacong@ucsc.edu>
2021-11-20 09:36:55 +0000

    attempt fix makepub

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -16,8 +16,7 @@ void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t i
 
     /* calculate numbers needed for bits that go to p and q */
     pbits = rand() % ((2 * nbits) / 4);
-    pbits = pbits + nbits;
-    pbits = (pbits / 4);
+    pbits = pbits + (nbits / 4);
 
     qbits = nbits - pbits + 1;
 
