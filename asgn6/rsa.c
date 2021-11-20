251a3aed2bf13e8cdd40675bedfdc14d6fbc0569
nacong <nacong@ucsc.edu>
2021-11-20 10:06:15 +0000

    attempt fix makepub

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -18,6 +18,8 @@ void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t i
     pbits = rand() % ((2 * nbits) / 4) + (nbits / 4);
     qbits = nbits - pbits;
 
+    pbits += 1;
+
     /* make prime numbers */
     make_prime(p, pbits, iters);
     make_prime(q, qbits, iters);
