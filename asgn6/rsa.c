400d7f97da55fb24cee74ef75d2ee88622bbead4
nacong <nacong@ucsc.edu>
2021-11-21 05:50:07 +0000

    tweak encrypt file and decrypt file

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -142,7 +142,7 @@ void rsa_decrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t d) {
     int reading = 0;
     while ((reading = gmp_fscanf(infile, "%Zx\n", ciphertext)) > 0) {
         rsa_decrypt(message, ciphertext, d, n);
-	mpz_export(buf, &reading, 1, blockint - 1, 1, 0, message);
+	mpz_export(buf, reading, 1, blockint - 1, 1, 0, message);
         fwrite(&buf[1], sizeof(uint8_t), blockint - 1, outfile);
     }
 
