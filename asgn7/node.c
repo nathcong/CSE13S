2754af208acbcb551419f5115c8e3c6d3aba25cf
nacong <nacong@ucsc.edu>
2021-12-04 07:30:39 +0000

    node.c

--- asgn7/node.c
+++ asgn7/node.c
@@ -1,10 +1,32 @@
 #include "node.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+Node *node_create(char *oldspeak, char *newspeak) {
+	Node *n = (Node *) malloc(sizeof(Node));
+	if (n) {
+		n->oldspeak = strdup(oldspeak);
+		n->newspeak = strdup(newspeak);
+		n->left = NULL;
+		n->right = NULL;
+	}
+	return n;
+}
 
-Node *node_create(char *oldspeak, char *newspeak);
+void node_delete(Node **n) {
+	free(*n);
+	*n = NULL;
+	return;
+}
 
-void node_delete(Node **n);
-
-void node_print(Node *n);
+void node_print(Node *n) {
+	if (n) {
+		printf("%s", n->oldspeak);
+		printf("%s", n->newspeak);
+		node_print(n->left);
+		node_print(n->right);
+	}
+}
 
